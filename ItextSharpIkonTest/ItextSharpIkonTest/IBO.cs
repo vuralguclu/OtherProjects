59703de56f5fd6c9bc87d0fa6d7ca1376dcffcdc
vuralguclu <vuralguclu@gmail.com>
2017-10-03 10:55:27 +0300

    ItextSharpIkonTest
    Create image and add it to pdf with itextsharp

---
+++ ItextSharpIkonTest/ItextSharpIkonTest/IBO.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Drawing.Imaging;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace ItextSharpIkonTest
+{
+    [Serializable]
+    public class PdfUpdateParameters
+    {
+        /// <summary>
+        /// The actual file to be update
+        /// </summary>
+        public byte[] PDFContent { get; set; }
+
+        /// <summary>
+        /// Name of the document updated
+        /// </summary>
+        public string PDFName { get; set; }
+
+        /// <summary>
+        /// information whether all pages will be printed
+        /// </summary>
+        public bool ToAllPages { get; set; }
+
+        /// <summary>
+        /// information of which pages will be printed
+        /// </summary>
+        public List<int> PageNumbers { get; set; }
+
+        /// <summary>
+        /// information to be printed
+        /// </summary>
+        public InfoToBePrinted Info { get; set; }
+
+
+    }
+
+    [Serializable]
+    public class InfoToBePrinted
+    {
+        /// <summary>
+        /// Confirmation Info
+        /// </summary>
+        public string ConfirmationInfo { get; set; }
+
+        /// <summary>
+        /// Font Name
+        /// </summary>
+        public string FontName { get; set; }
+
+        /// <summary>
+        /// Font Size
+        /// </summary>
+        public int FontSize { get; set; }
+
+        /// <summary>
+        /// HTML Color
+        /// </summary>
+        public string HTMLColor { get; set; }
+
+        /// <summary>
+        /// xCoordinate
+        /// </summary>
+        public int PDF_X { get; set; }
+
+        /// <summary>
+        /// yCoordinate
+        /// </summary>
+        public int PDF_Y { get; set; }
+
+        /// <summary>
+        /// width
+        /// </summary>
+        public int TextWidth { get; set; }
+    }
+
+    [Serializable]
+    public class PdfUpdateOperationResult
+    {
+        /// <summary>
+        /// Property to set success or failure message
+        /// </summary>
+        public string Status { get; set; }
+
+        /// <summary>
+        /// Size of the downloaded document
+        /// </summary>
+        public string NewDocumentSize { get; set; }
+
+        /// <summary>
+        /// Content of the downloaded document
+        /// </summary>
+        public byte[] NewDocumentFile { get; set; }
+
+    }
+
+    public class IBO
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.FileManagerServiceSoapClient client = new ServiceReference1.FileManagerServiceSoapClient();
+
+            ServiceReference1.PdfUpdateParameters pdfParameters = new ServiceReference1.PdfUpdateParameters();
+            pdfParameters.PageNumbers = new ServiceReference1.ArrayOfInt();
+            pdfParameters.PageNumbers.Add(1);
+            pdfParameters.PageNumbers.Add(3);
+            pdfParameters.PageNumbers.Add(5);
+            pdfParameters.PDFName = "PdfTest.pdf";
+            pdfParameters.PDFContent = File.ReadAllBytes(@"C:\PdfTest.pdf");
+            pdfParameters.ToAllPages = true;
+            pdfParameters.Info = new ServiceReference1.InfoToBePrinted();
+            pdfParameters.Info.ConfirmationInfo = "hop hop";
+            pdfParameters.Info.FontName = "arial";
+            pdfParameters.Info.FontSize = 10;
+            pdfParameters.Info.HTMLColor = "#FF0000";
+            pdfParameters.Info.PDF_X = 100;
+            pdfParameters.Info.PDF_Y = 100;
+            ServiceReference1.PdfUpdateOperationResult result = client.PdfUpdate(pdfParameters);
+            File.WriteAllBytes(@"C:\newnew.pdf", result.NewDocumentFile);
+            client.Close();
+        }
+
+        public static PdfUpdateOperationResult PdfUpdate(PdfUpdateParameters updateDocParameters)
+        {
+            PdfUpdateOperationResult result = new PdfUpdateOperationResult();
+            System.Drawing.Font font = CreateFont(updateDocParameters.Info.FontName,updateDocParameters.Info.FontSize);
+            System.Drawing.Color color = CreateColor(updateDocParameters.Info.HTMLColor);
+            
+            byte[] png = DrawTextToPng(updateDocParameters.Info.ConfirmationInfo, font, color, updateDocParameters.Info.TextWidth);
+            byte[] newPdf = CreatePdf(updateDocParameters.PDFContent, png, updateDocParameters.Info.PDF_X, updateDocParameters.Info.PDF_Y, updateDocParameters.ToAllPages, updateDocParameters.PageNumbers);
+
+            return result;
+        }
+
+        private static System.Drawing.Color CreateColor(string colorCode)
+        {
+            bool existsColor = System.Text.RegularExpressions.Regex.IsMatch(colorCode, @"^#(?:[0-9a-fA-F]{3}){1,2}$");
+
+            if (existsColor)
+            {
+                return System.Drawing.ColorTranslator.FromHtml(colorCode);
+            }
+            else
+            {
+                return System.Drawing.ColorTranslator.FromHtml("#000000");
+            }
+        }
+
+        private static System.Drawing.Font CreateFont(string fontName, int fontSize)
+        {
+            System.Drawing.Font font = new System.Drawing.Font(fontName, fontSize);
+
+            if (font.Name.ToLower() != fontName.ToLower())
+            {
+                if (fontSize <= 0)
+                {
+                    font = new System.Drawing.Font("arial", 10);
+                }
+                else
+                {
+                    font = new System.Drawing.Font("arial", fontSize);
+                }
+            }
+
+            return font;
+        }
+
+        private static bool CheckPageNumbers(bool toAllPages, List<int> pageNumbers, int numberOfPages)
+        {
+            if (toAllPages)
+            {
+                if (numberOfPages > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                for (int index = 0; index < pageNumbers.Count; index++)
+                {
+                    if (pageNumbers[index] > numberOfPages || pageNumbers[index] < 1)
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+
+        private static byte[] DrawTextToPng(String text, System.Drawing.Font font, System.Drawing.Color textColor, int maxWidth)
+        {
+            System.Drawing.Image img = new Bitmap(1, 1);
+            Graphics drawing = Graphics.FromImage(img);
+            SizeF textSize;
+
+            if (maxWidth <= 0)
+            {
+                textSize = drawing.MeasureString(text, font);
+            }
+            else
+            {
+                textSize = drawing.MeasureString(text, font, maxWidth);
+            }
+            
+            StringFormat sf = new StringFormat();
+            Brush textBrush = new SolidBrush(textColor);
+
+            sf.Trimming = StringTrimming.Word;
+            img.Dispose();
+            drawing.Dispose();
+            img = new Bitmap((int)textSize.Width, (int)textSize.Height);
+
+            drawing = Graphics.FromImage(img);
+            drawing.CompositingQuality = CompositingQuality.HighQuality;
+            drawing.InterpolationMode = InterpolationMode.HighQualityBilinear;
+            drawing.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            drawing.SmoothingMode = SmoothingMode.HighQuality;
+            drawing.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+            drawing.Clear(System.Drawing.Color.Transparent);
+            drawing.DrawString(text, font, textBrush, new RectangleF(0, 0, textSize.Width, textSize.Height), sf);
+            drawing.Save();
+
+            textBrush.Dispose();
+            drawing.Dispose();
+            
+            MemoryStream pngStream = new MemoryStream();
+            img.Save(pngStream, ImageFormat.Png);
+            img.Dispose();
+            return pngStream.ToArray();
+        }
+
+        private static byte[] CreatePdf(byte[] pdfIn, byte[] pngIn, int xCoordinate, int yCoordinate, bool toAllPages, List<int> pageNumbers)
+        {
+            MemoryStream destinationStream = new MemoryStream();
+            iTextSharp.text.pdf.PdfReader reader = null;
+            iTextSharp.text.pdf.PdfStamper stamper = null;
+
+            try
+            {
+                reader = new iTextSharp.text.pdf.PdfReader(pdfIn);
+                int numberOfPages = reader.NumberOfPages;
+
+                if (CheckPageNumbers(toAllPages, pageNumbers, numberOfPages) == false)
+                {
+                    return null;//HATA - geçersiz sayfa sayısı
+                }
+                
+                if (toAllPages)
+                {
+                    pageNumbers = new List<int>();
+
+                    for (int index = 1; index <= numberOfPages; index++)
+                    {
+                        pageNumbers.Add(index);
+                    }
+                }
+
+                stamper = new iTextSharp.text.pdf.PdfStamper(reader, destinationStream, '\0', true);
+
+                foreach (int pageNumber in pageNumbers)
+                {
+                    int posX = Convert.ToInt32(xCoordinate);
+                    int posY = Convert.ToInt32(yCoordinate);
+                    string stampName = "Page" + pageNumber.ToString();
+
+                    iTextSharp.text.Image image = iTextSharp.text.Image.GetInstance(pngIn);
+                    image.SetAbsolutePosition(0, 0);
+
+                    iTextSharp.text.pdf.PdfTemplate template = iTextSharp.text.pdf.PdfTemplate.CreateTemplate(stamper.Writer, image.Width, image.Height);
+                    template.AddImage(image);
+
+                    iTextSharp.text.Rectangle rect = new iTextSharp.text.Rectangle(xCoordinate, yCoordinate, xCoordinate + image.Width, yCoordinate - image.Height);
+                    iTextSharp.text.pdf.PdfAnnotation annotation = iTextSharp.text.pdf.PdfAnnotation.CreateStamp(stamper.Writer, rect, null, stampName);
+                    annotation.SetAppearance(iTextSharp.text.pdf.PdfName.N, template);
+                    annotation.Flags = iTextSharp.text.pdf.PdfAnnotation.FLAGS_PRINT;
+                    stamper.AddAnnotation(annotation, pageNumber);
+                }
+
+                reader.Close();
+                stamper.Close();
+                return destinationStream.ToArray();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (destinationStream != null)
+                    destinationStream.Close();
+            }
+        }
+    }
+}
---
+++ ItextSharpIkonTest/ItextSharpIkonTest/Program.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Collections;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace ItextSharpIkonTest
+{
+    /*EdiTT'teki kodun taklidi*/
+    public class Bookmark
+    {
+        public string Name { get; private set; }
+        public string Action { get; private set; }
+        public string Page { get; private set; }
+        public PageInfo Info { get; private set; }
+
+        public Bookmark(string name, string action, string page)
+        {
+            this.Name = name;
+            this.Action = action;
+            this.Page = page;
+            this.Info = new PageInfo(page);
+        }
+    }
+    /*EdiTT'teki kodun taklidi*/
+    public class PageInfo
+    {
+        public int PageId { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public PageInfo(string page)
+        {
+            try
+            {
+                string[] m_values = page.Split(' ');
+                this.PageId = Convert.ToInt32(m_values[0].ToString());
+                this.X = Convert.ToInt32(m_values[2].ToString());
+                this.Y = Convert.ToInt32(m_values[3].ToString());
+            }
+            catch
+            {
+
+                throw;
+            }
+        }
+    }
+    /*EdiTT'teki kodun taklidi*/
+    public class PdfBookmark
+    {
+        /*EdiTT'teki kodun taklidi*/
+        public List<Bookmark> GetBookmark(byte[] pdfIn, byte[] ownerPassword)
+        {
+            List<Bookmark> m_bookmarks = new List<Bookmark>();
+            myTextSharp.text.pdf.PdfReader m_reader = null;
+            try
+            {
+                m_reader = new myTextSharp.text.pdf.PdfReader(pdfIn, ownerPassword);
+                IList<Dictionary<string, object>> bookmarks = myTextSharp.text.pdf.SimpleBookmark.GetBookmark(m_reader);
+
+                if (bookmarks != null)
+                {
+                    if (bookmarks.Count > 0)
+                    {
+
+                        foreach (Dictionary<string, object> m_bookmark in bookmarks)
+                        {
+                            string m_title = string.Empty;
+                            string m_action = string.Empty;
+                            string m_page = string.Empty;
+
+                            foreach (var item in m_bookmark)
+                            {
+                                switch (item.Key)
+                                {
+                                    case "Title":
+                                        m_title = item.Value.ToString();
+                                        break;
+                                    case "Action":
+                                        m_action = item.Value.ToString();
+                                        break;
+                                    case "Page":
+                                        m_page = item.Value.ToString();
+                                        break;
+                                }
+                            }
+                            m_bookmarks.Add(new Bookmark(m_title, m_action, m_page));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                m_bookmarks = null;
+            }
+            finally
+            {
+                if (m_reader != null)
+                {
+                    m_reader.Close();
+                }
+            }
+            return (m_bookmarks);
+        }
+
+        /*EdiTT'teki kodun taklidi*/
+        public List<Bookmark> GetBookmarkEx(iTextSharp.text.pdf.PdfReader m_reader, byte[] pdfIn)
+        {
+            List<Bookmark> m_bookmarks = new List<Bookmark>();
+            try
+            {
+                ArrayList bookmarks = iTextSharp.text.pdf.SimpleBookmark.GetBookmark(m_reader);
+
+                foreach (var item in bookmarks.ToArray())
+                {
+                    Hashtable map = item as Hashtable;
+                    string m_title = string.Empty;
+                    string m_action = string.Empty;
+                    string m_page = string.Empty;
+                    m_title = map["Title"].ToString();
+                    m_action = map["Action"].ToString();
+                    m_page = map["Page"].ToString();
+
+
+                    m_bookmarks.Add(new Bookmark(m_title, m_action, m_page));
+
+                }
+            }
+            catch (Exception ex)
+            {
+                m_bookmarks = null;
+            }
+            return (m_bookmarks);
+        }
+
+        /*BU YENİ METOT, verilen bookmarkların olduğu yere verilen imajı basıyor*/
+        public void InsertAbsolutePositionIcon(byte[] pdfIn, byte[] iconIn, List<KeyValuePair<long, KeyValuePair<string, string>>> bookMCoord)
+        {
+            using (FileStream outputPdfStream = new FileStream(@"D:\Folder\NewPDF_" + DateTime.Now.Ticks.ToString() + ".pdf", FileMode.Create, FileAccess.Write))
+            {
+                PdfReader reader = null;
+                PdfStamper stamper = null;
+                try
+                {
+                    reader = new PdfReader(pdfIn);
+                    stamper = new PdfStamper(reader, outputPdfStream, '\0', true);
+
+                    foreach (KeyValuePair<long, KeyValuePair<string, string>> kv in bookMCoord)
+                    {
+                        string[] imzaCoord = kv.Value.Value.Split(';');
+                        int pageNumber = Convert.ToInt32(imzaCoord[0]);
+                        int posX = Convert.ToInt32(imzaCoord[1]);
+                        int posY = Convert.ToInt32(imzaCoord[2]);
+                        //string stampName = "ImzaOnayKutusu" + kv.Key; //burada kv.Key hep 1 verdim ve unique olması lazım, gerçekte aktorId var bu nedenle alt satır açık bu commentli
+                        string stampName = "ImzaOnayKutusu" + kv.Key.ToString() + posX.ToString() + posY.ToString(); 
+
+                        int x = 0;
+                        int y = 25;
+                        int h = 30;
+                        int w = 30;
+
+                        x = posX + 50;
+                        y = posY - 5;
+
+                        Image image = Image.GetInstance(iconIn);
+                        image.SetAbsolutePosition(0, 0);
+
+                        PdfTemplate template = PdfTemplate.CreateTemplate(stamper.Writer, image.Width, image.Height);
+                        template.AddImage(image);
+
+                        Rectangle rect = new Rectangle(x, y, x + w, y-h);
+                        PdfAnnotation annotation = PdfAnnotation.CreateStamp(stamper.Writer, rect, null, stampName);
+                        annotation.SetAppearance(PdfName.N, template);
+                        annotation.Flags = PdfAnnotation.FLAGS_PRINT;
+                        stamper.AddAnnotation(annotation, pageNumber);
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    reader.Close();
+                    stamper.Close();
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*BU KISIM new TaslakYonetimiBusiness().GetBookmarkCoordinate(item); kodunu taklit ediyor.*/
+            byte[] pdfIn  = File.ReadAllBytes(@"D:\Folder\5081f98763b14fba922673b8ad624d7a.pdf");
+            byte[] iconIn = ExtractResource(@"ItextSharpIkonTest.icons.onaylogo.jpg");
+
+            List<KeyValuePair<long, KeyValuePair<string, string>>> bookMCoord = new List<KeyValuePair<long, KeyValuePair<string, string>>>();
+            List<Bookmark> bookmarkList = new PdfBookmark().GetBookmark(pdfIn, null);
+
+            if (bookmarkList != null && bookmarkList.Count > 0)
+            {
+                foreach (Bookmark bookm in bookmarkList)
+                {
+                    if (bookm.Name.StartsWith("bm_"))
+                    {
+                        string[] bookMArr = bookm.Name.Split('_');
+                        KeyValuePair<string, string> tempPair = new KeyValuePair<string, string>(bookMArr[1], bookm.Info.PageId + ";" + bookm.Info.X + ";" + bookm.Info.Y);
+                        bookMCoord.Add(new KeyValuePair<long, KeyValuePair<string, string>>(Convert.ToInt32("1"), tempPair));
+                    }
+                }
+            }
+
+            /*********************************************/
+
+            /*ASIL İŞİ YAPAN YENİ METOT!!!!*/
+            new PdfBookmark().InsertAbsolutePositionIcon(pdfIn, iconIn, bookMCoord);
+
+            
+
+        }
+
+        /*Embedded Resource Okuma ÖRNEĞİ*/
+        public static byte[] ExtractResource(String filename)
+        {
+            System.Reflection.Assembly a = System.Reflection.Assembly.GetExecutingAssembly();
+            using (Stream resFilestream = a.GetManifestResourceStream(filename))
+            {
+                if (resFilestream == null) return null;
+                byte[] ba = new byte[resFilestream.Length];
+                resFilestream.Read(ba, 0, ba.Length);
+                return ba;
+            }
+        }
+    }
+}
